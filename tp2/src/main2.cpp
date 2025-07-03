0ada94b3f668b2da2946678fc59b5e965691c364
Daniel Puyol <danielignaciopuyol@gmail.com>
2025-07-03 19:42:51 -0300

    grasp

--- tp2/src/main2.cpp
+++ tp2/src/main2.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(){
 
-    VRPLIBReader instance("/workspaces/TP2-TD5/tp2/instancias/2l-cvrp-0/E016-03m.dat");
+    VRPLIBReader instance("/workspaces/TP2-TD5/tp2/instancias/2l-cvrp-0/E016-05m.dat");
 
     GRASP test;
     Solucion s = test.resolver(5,10,instance);
