7a5a5c26b7e2d9a0289b331da1270974474593b5
Daniel Puyol <danielignaciopuyol@gmail.com>
2025-07-03 19:52:16 -0300

    grasp

--- tp2/src/main2.cpp
+++ tp2/src/main2.cpp
@@ -12,11 +12,8 @@ int main(){
     VRPLIBReader instance("/workspaces/TP2-TD5/tp2/instancias/2l-cvrp-0/E016-05m.dat");
 
     GRASP test;
-    Solucion s = test.resolver(5,10,instance);
+    Solucion s = test.resolver(4,1000,instance);
     s.printSolucion();
-    cout<<endl;
-    VMC test2;
-    Solucion s2 = test2.resolver(instance);
-    s2.printSolucion();
+    
     return 0;
 }
